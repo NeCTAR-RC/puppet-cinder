a0385e0c2ff895bf5d46b26b11f0c621fc1e13cb
Sam Morrison <sorrison@gmail.com>
2012-12-03 14:45:49 +1100

    Ngix frontend for cinder, nagios checks

--- manifests/api.pp
+++ manifests/api.pp
@@ -6,9 +6,37 @@ class cinder::api inherits cinder {
 
   service {'cinder-api':
     ensure    => running,
-    subscribe => [ File['/etc/cinder/cinder.conf']
+    subscribe => [ File['/etc/cinder/cinder.conf'],
                    File['/etc/cinder/api-paste.ini']],
     require   => Package['cinder-api'],
+  }
+
+  nagios::service {
+    'http_openstack_18776':
+      check_command => 'http_port!18776';
+  }
+
+  nagios::nrpe::service { 'service_cinder_api':
+    check_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cinder -a /usr/bin/cinder-api';
+  }
+
+}
+
+class cinder::api::load-balanced inherits cinder::api {
+
+  include nginx
+
+  nginx::proxy { 'cinder':
+    port         => 8776,
+    ssl          => true,
+    upstreams    => $cinder_upstream,
+    nagios_check => false,
+  }
+
+  nagios::service {
+    'http_openstack_8776':
+      check_command => 'https_port!8776',
+      servicegroups => 'openstack-endpoints';
   }
 
 }
