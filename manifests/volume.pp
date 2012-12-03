efebf5abe1634bd9a2d4474fefbf02ffe350ca16
Sam Morrison <sorrison@gmail.com>
2012-12-03 14:46:31 +1100

    nagios checks

--- manifests/volume.pp
+++ manifests/volume.pp
@@ -10,4 +10,9 @@ class cinder::volume inherits cinder {
                    File['/etc/cinder/api-paste.ini']],
     require   => Package['cinder-volume'],
   }
+
+  nagios::nrpe::service { 'service_cinder_volume':
+    check_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cinder -a /usr/bin/cinder-volume';
+  }
+
 }
