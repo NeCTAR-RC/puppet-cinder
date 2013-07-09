f385e180411e8caa93abc265ce67743e8617f352
Sam Morrison <sorrison@gmail.com>
2013-07-09 14:40:20 +1000

    Support workers to allow multiple cinder-apis
    Change-Id: If085a353671916c3570162646513e7daeeb718dd

--- manifests/api.pp
+++ manifests/api.pp
@@ -1,11 +1,11 @@
-class cinder::api inherits cinder {
+class cinder::api($service_ensure='running') inherits cinder {
 
   package {'cinder-api':
     ensure => installed,
   }
 
   service {'cinder-api':
-    ensure    => running,
+    ensure    => $service_ensure,
     subscribe => [ File['/etc/cinder/cinder.conf'],
                    File['/etc/cinder/api-paste.ini']],
     require   => Package['cinder-api'],
@@ -19,5 +19,29 @@ class cinder::api inherits cinder {
   nagios::nrpe::service { 'service_cinder_api':
     check_command => '/usr/lib/nagios/plugins/check_procs -c 1:20 -u cinder -a /usr/bin/cinder-api';
   }
+
+  define worker() {
+    file { "/etc/cinder/cinder-api-${name}.conf":
+      ensure  => present,
+      owner   => cinder,
+      group   => cinder,
+      mode    => '0600',
+      content => "[DEFAULT]\nosapi_volume_listen_port = ${name}",
+      notify  => Service["cinder-api-${name}"],
+    }
+
+    file {"/etc/init/cinder-api-${name}.conf":
+      content => template('cinder/api-init.conf.erb'),
+    }
+
+    service {"cinder-api-${name}":
+      ensure    => running,
+      provider  => upstart,
+      subscribe => [ File['/etc/cinder/cinder.conf'],
+                     File['/etc/cinder/api-paste.ini']],
+      require   => File["/etc/init/cinder-api-${name}.conf"],
+    }
+  }
+
 
 }
