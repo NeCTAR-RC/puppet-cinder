e2c442ed7332a8891a37d25bf3d52274a87303af
Edan Scriven <edan.scriven@uqconnect.edu.au>
2013-07-09 13:36:01 +1000

    Changed nagios check service_cinder_volume.
    This time the Nagios check simply looks for 1-20 processes.
    
    Change-Id: I7b9aa5a2a4c7855cdaac1222fdd4a62b1ac3090e

--- manifests/volume.pp
+++ manifests/volume.pp
@@ -12,7 +12,7 @@ class cinder::volume inherits cinder {
   }
 
   nagios::nrpe::service { 'service_cinder_volume':
-    check_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cinder -a /usr/bin/cinder-volume';
+    check_command => '/usr/lib/nagios/plugins/check_procs -c 1:20 -u cinder -a /usr/bin/cinder-volume';
   }
 
 }
