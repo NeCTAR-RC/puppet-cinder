b7a9f23d5032ac3ad74a5963b97a2fb64e27b02d
Edan Scriven <edan.scriven@uqconnect.edu.au>
2013-07-09 12:18:37 +1000

    Changed nagios check service_cinder_volume.

--- manifests/volume.pp
+++ manifests/volume.pp
@@ -1,4 +1,4 @@
-class cinder::volume inherits cinder {
+class cinder::volume($volume_procs='1') inherits cinder {
 
   package {'cinder-volume':
     ensure => installed,
@@ -11,8 +11,10 @@ class cinder::volume inherits cinder {
     require   => Package['cinder-volume'],
   }
 
+  $volume_procs = hiera('cinder::volume::processes')
+
   nagios::nrpe::service { 'service_cinder_volume':
-    check_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u cinder -a /usr/bin/cinder-volume';
+    check_command => '/usr/lib/nagios/plugins/check_procs -c ${procs}:${procs} -u cinder -a /usr/bin/cinder-volume';
   }
 
 }
