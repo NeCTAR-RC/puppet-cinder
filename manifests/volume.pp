ec901bc817a6041629c29bd9bf9361cac73d9bd6
Sam Morrison <sorrison@gmail.com>
2013-10-04 11:08:19 +1000

    Ensure packages for image copying installed, ceilometer notifications
    Change-Id: I2d57c53bbc0abfd1fce29ff741ab58f4f6035ca9

--- manifests/volume.pp
+++ manifests/volume.pp
@@ -15,4 +15,16 @@ class cinder::volume inherits cinder {
     check_command => '/usr/lib/nagios/plugins/check_procs -c 1:20 -u cinder -a /usr/bin/cinder-volume';
   }
 
+  package {['qemu-utils', 'open-iscsi']:
+    ensure => installed,
+  }
+
+  cron { 'cinder-volume-usage-audit':
+    ensure  => present,
+    command => '/usr/bin/cinder-volume-usage-audit >/dev/null 2>&1',
+    user    => 'root',
+    minute  => 5,
+    require => Package['cinder-volume'],
+  }
+
 }
