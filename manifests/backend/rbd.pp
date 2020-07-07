b03da3f2df3c79253e30b7bc7640354ae49d4ae2
Shi Yan <shi.yan@unimelb.edu.au>
2020-07-07 15:17:09 +1000

    Support to manage packages for ceph backend
    Change-Id: I42092f75f05f1f6892f15a2e57567578613365bb

--- manifests/backend/rbd.pp
+++ manifests/backend/rbd.pp
@@ -78,6 +78,10 @@
 #   with volume_backend_name=$volume_backend_name key/value.
 #   Defaults to false.
 #
+# [*manage_package*]
+#   (Optional) Whether or not manage Cinder support package.
+#   Defaults to true.
+#
 # [*extra_options*]
 #   (optional) Hash of extra options to pass to the backend stanza
 #   Defaults to: {}
@@ -102,6 +106,7 @@ define cinder::backend::rbd (
   $rbd_exclusive_cinder_pool        = $facts['os_service_default'],
   Boolean $manage_volume_type       = false,
   Hash $extra_options               = {},
+  Boolean $manage_package           = true,
 ) {
 
   include cinder::deps
@@ -155,10 +160,12 @@ define cinder::backend::rbd (
     }
   }
 
-  ensure_packages( 'ceph-common', {
-    ensure => present,
-    name   => $::cinder::params::ceph_common_package_name,
-    tag    => 'cinder-support-package'})
+  if $manage_package {
+    ensure_packages( 'ceph-common', {
+      ensure => present,
+      name   => $::cinder::params::ceph_common_package_name,
+      tag    => 'cinder-support-package'})
+  }
 
   create_resources('cinder_config', $extra_options)
 
